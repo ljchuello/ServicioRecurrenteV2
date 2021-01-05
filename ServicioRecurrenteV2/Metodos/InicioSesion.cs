374716a2df4f8fdbfbe22c3ec790f9348d921aec
Leonardo Chuello <ljchuello@gmail.com>
2021-01-05 15:37:08 -0500

    ya hace login

--- ServicioRecurrenteV2/Metodos/InicioSesion.cs
+++ ServicioRecurrenteV2/Metodos/InicioSesion.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using RestSharp;
 
 namespace ServicioRecurrenteV2.Metodos
@@ -41,22 +42,9 @@ namespace ServicioRecurrenteV2.Metodos
                 var client = new RestClient("http://local.sermatick.com:8080/Account/Login?ReturnUrl=%2F");
                 client.Timeout = -1;
                 var request = new RestRequest(Method.POST);
-                request.AddHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-                request.AddHeader("Accept-Encoding", "gzip, deflate");
-                request.AddHeader("Accept-Language", "es-ES,es;q=0.9");
-                request.AddHeader("Cache-Control", "max-age=0");
-                request.AddHeader("Connection", "Close");
-                request.AddHeader("Content-Length", "175");
-                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
-                request.AddHeader("Cookie", $"ASP.NET_SessionId={SessionId}; __RequestVerificationToken={RequestVerificationTokenCookie}");
-                request.AddHeader("DNT", "1");
-                request.AddHeader("Host", "local.sermatick.com:8080");
-                request.AddHeader("Origin", "http://local.sermatick.com:8080");
-                request.AddHeader("Referer", "http://local.sermatick.com:8080/Account/Login?ReturnUrl=%2F");
-                request.AddHeader("Upgrade-Insecure-Requests", "1");
-                client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
-                
-                request.AddParameter("__RequestVerificationToken", $"{RequestVerificationTokenBody}");
+                request.AddParameter("ASP.NET_SessionId", SessionId, ParameterType.Cookie);
+                request.AddParameter("__RequestVerificationToken", RequestVerificationTokenCookie, ParameterType.Cookie);
+                request.AddParameter("__RequestVerificationToken", RequestVerificationTokenBody);
                 request.AddParameter("Email", "admin@admin.com");
                 request.AddParameter("Password", "123456");
                 IRestResponse response = client.Execute(request);
