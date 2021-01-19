f607605f15f934c4bc0cb372835159a83bfe478c
Leonardo Chuello <ljchuello@gmail.com>
2021-01-19 16:30:36 -0500

    .

---
+++ VisualV12/Libreria/Servicios.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualV12.Libreria
+{
+    public class Servicios
+    {
+        public bool ExisteServ(string servicio)
+        {
+            try
+            {
+                ServiceController[] services = ServiceController.GetServices();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
